80f5940345f5cb6ad321a19be31c9542b4fa14e2
yuli.zhang <2285546178@qq.com>
2023-02-05 14:44:36 +0800

    ✨ feat(css): add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,54 @@
 import React,{ useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import { Layout } from 'antd';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { Header, Footer, Sider, Content } = Layout;
+  const layoutStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#3ba0e9',
+  };
+  const headerStyle: React.CSSProperties = {
+    display:'flex',
+    width: '100%',
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 50,
+    lineHeight: '64px',
+    backgroundColor: '#7dbcea',
+  };
+  const siderStyle: React.CSSProperties = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#3ba0e9',
+  };
+  const contentStyle: React.CSSProperties = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#108ee9',
+  };
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>
+          在下方填写信息，即可在右边预览会生成的样子
+        </Header>
+      <Layout style={layoutStyle}>
+        <Sider style={siderStyle}/>
+        <Content style={contentStyle}/>
+      </Layout>
+      <Footer />
+      </Layout>
+
     </div>
   )
 }
